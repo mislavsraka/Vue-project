4dfa8d1ea979953d30ad6bf3d8c3a55b76e5b0e7
Mislav Sraka <mislavsraka@gmail.com>
2019-11-19 15:32:02 +0100

    First Vue app

--- src/main.js
+++ src/main.js
@@ -8,8 +8,13 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+	el: '#app',
+  data: {
+  	title: 'Hello World!'
+  },
+  methods: {
+  	changeTitle: function(event) {
+    	this.title = event.target.value;
+    }
+  }
+});
