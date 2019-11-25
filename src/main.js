0ce5439668d80ffbeba706d398e71d57072ba0d8
Mislav Sraka <mislavsraka@gmail.com>
2019-11-25 14:25:05 +0100

    Added mouse coordinates

--- src/main.js
+++ src/main.js
@@ -10,11 +10,17 @@ Vue.config.productionTip = false
 new Vue({
 	el: '#app',
   data: {
-  	counter: 0
+  	counter: 0,
+  	x: 0,
+  	y: 0
   },
   methods: {
   	increase: function() {
   		this.counter++;
+    },
+    updateCoordinates: function(event) {
+    	this.x = event.clientX;
+    	this.y = event.clientY;
     }
   }
 });
