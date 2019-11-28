3de5613abeb4b8c8c5b2f07ed3a75b29942c5188
Mislav Sraka <mislavsraka@gmail.com>
2019-11-28 13:36:55 +0100

    Excercise_2 done

--- src/main.js
+++ src/main.js
@@ -8,22 +8,13 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-	el: '#app',
+	el: '#exercise',
   data: {
-  	counter: 0,
-  	x: 0,
-  	y: 0
+  	value: ''
   },
   methods: {
-  	increase: function(step, event) {
-  		this.counter += step;
-    },
-    updateCoordinates: function(event) {
-    	this.x = event.clientX;
-    	this.y = event.clientY;
-    },
-	alertMe: function() {
-		alert('Alert!');
-	}
+  	alertMe: function() {
+  		alert('Alert!');
+    }
   }
 });
