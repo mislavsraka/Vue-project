f7ab70bf18b1e77aacb271730d05def35c2a2020
Mislav Sraka <mislavsraka@gmail.com>
2019-12-04 15:40:21 +0100

    Done exercise 5 object for

--- src/main.js
+++ src/main.js
@@ -11,39 +11,17 @@ Vue.config.productionTip = false
 new Vue({
 	el: '#exercise',
   data: {
-  	effectClasses: { 
-  		highlight: false, 
-  		shrink: true
-  	},
-    float: 'float',
-    userClass: '',
-    isVisible: true,
-    myStyle: {
-      width: '100px',
-      height: '150px',
-      backgroundColor: 'gray'
+  	isShown: true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3',
     },
-    progressBar: {
-      width: '0px',
-      backgroundColor: 'red'
-    }
-  },
-  methods: {
-    startEffect: function() {
-     var vm = this;
-      setInterval(function() {
-      vm.effectClasses.highlight = !vm.effectClasses.highlight;
-        vm.effectClasses.shrink = !vm.effectClasses.shrink;
-      }, 1000);
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
     },
-    startProgress: function() {
-    var vm = this;
-      var width = 0;
-     
-    setInterval(function() {
-      width = width + 10;
-      vm.progressBar.width = width + 'px';
-      }, 500);
-    }
   }
 });
